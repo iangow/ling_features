8f736f7eeb091003d9c405e1d491b05d8f0990bd
Ian Gow <iandgow@gmail.com>
2020-05-12 13:05:55 -0400

    Fixes to tone function.

--- tone/tone_measure_functions.py
+++ tone/tone_measure_functions.py
@@ -2,17 +2,15 @@ import re
 
 file = open("tone/word_lists.txt", "r")
 word_lists = eval(file.read())
+categories = [key for key in word_lists.keys()]
 
 def make_regex(word_list):
     regex_text = '\\b(?:' + '|'.join(word_list) + ')\\b'
     regex = re.compile(regex_text)
     
     return regex
-
-def re_dict():
-    categories = [key for key in word_lists.keys()]
-    regex_dict = {cat: make_regex(word_lists[cat]) for cat in categories }
-    return regex_dict
+    
+regex_dict = {cat: make_regex(word_lists[cat]) for cat in categories }
 
 def tone_count(the_text):
     """Function to return number of matches in a category in a text"""
