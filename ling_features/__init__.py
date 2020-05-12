f3a1f8eea97460b45f25d4333006ae697a446171
Ian Gow <iandgow@gmail.com>
2020-05-12 11:12:10 -0400

    Added missing comma.

--- ling_features/__init__.py
+++ ling_features/__init__.py
@@ -2,5 +2,5 @@ name = "ling_features"
 
 from fog.fog import fog
 
-from word_count.word_count_functions import word_count \
+from word_count.word_count_functions import word_count, \
   number_count, sent_count
