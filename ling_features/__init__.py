d01350dba6b83f3b8a3dbea7b14adad04b07293c
Ian Gow <iandgow@gmail.com>
2021-04-16 15:23:59 -0400

    Export get_regexes_df.

--- ling_features/__init__.py
+++ ling_features/__init__.py
@@ -11,4 +11,4 @@ from topic.topic_functions import kls_domains_ind
 
 from tone.tone_measure_functions import tone_count
 
-from non_answer.non_answers import non_answers, regexes
+from non_answer.non_answers import non_answers, get_regexes_df
